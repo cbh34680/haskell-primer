3d615a4ab8b1ba7ac53ffc055fcbd234e2004ee7
cbh34680 <cbh34680@gmail.com>
2024-08-16 22:34:15 +0900

    test

--- reader.hs
+++ reader.hs
@@ -1,9 +1,17 @@
--- hugs
-
 data Func r a = GenFunc { runFunc :: r -> a }
 
-instance Monad (Func x) where
-    return x = GenFunc $ \r -> x
+instance Functor (Func r) where
+    fmap f g = GenFunc $ \r -> let a = (runFunc g) r in f a
+
+instance Applicative (Func r) where
+    pure x = GenFunc $ \r -> x
+    m <*> n = GenFunc $ \r ->
+        let f = runFunc m r
+            x = runFunc n r
+        in f x
+                
+
+instance Monad (Func r) where
     m >>= f = GenFunc $ \r -> let a = (runFunc m) r in (runFunc (f a)) r
 
 fix :: Int -> Func a Int
@@ -17,10 +25,7 @@ step = (fix 1) >> ((arg) >>= (\b -> (return (length b))))
 
 f = runFunc step "abc"
 
+g = runFunc (fmap (+1) (return 1)) 1
 
-
-
-
-
-
+h = runFunc ((return (+1)) <*> (return 1)) 1
 

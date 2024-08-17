c5c051a6b9a88c056143f0632c16e89c0932aabc
cbh34680 <cbh34680@gmail.com>
2024-08-17 16:17:33 +0900

    test

--- state.hs
+++ state.hs
@@ -22,6 +22,28 @@ push x = GenState $ \s -> ((), x:s)
 pop :: State [a] a
 pop = GenState $ \(x:xs) -> (x, xs)
 
+get :: State s s
+get = GenState $ \s -> (s, s)
+
+put :: s -> State s ()
+put s = GenState $ \_ -> ((), s)
+
+gets :: (s -> a) -> State s a
+-- gets f = GenState $ \s -> (f s, s)
+gets f = do
+    s <- get
+    return $ f s
+
+modify :: (s -> s) -> State s ()
+-- modify f = GenState $ \s -> ((), f s)
+modify f = do
+    s <- get
+    put $ f s
+
+
+mapState :: ((a, s) -> (b, s)) -> State s a -> State s b
+mapState f m = GenState $ f . runState m
+
 
 f = runState (genState 1) []
 
@@ -30,5 +52,14 @@ g = runState (pure (+1) <*> pure 1) []
 h = runState step []
     where
         step = pure 1 >>= return . (+1) >>= push >> pop
+
+i = runState (mapState (\(a, s) -> (a+1, s+1)) (pure 1)) 1
+
+
+
+
+
+
+
 
 -- EOF

e701321b2173c86b8273a511e0be40e0c052d5a4
cbh34680 <cbh34680@gmail.com>
2024-10-17 12:54:13 +0900

    test

--- sequence.hs
+++ sequence.hs
@@ -6,3 +6,11 @@ sequence' (x:xs) = x >>= \y -> sequence' xs >>= \ys -> return (y:ys)
 f = sequence' [(+1), (+2)] 1
 
 
+sequence'' [] = return []
+
+sequence'' (x:xs) = do
+    y <- x
+    ys <- sequence'' xs
+    return (y:ys)
+
+g = sequence'' [(+1), (+2)] 1

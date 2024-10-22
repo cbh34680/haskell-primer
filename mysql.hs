6e03dbddbef80661c578fc067191d3b7e838c356
cbh34680 <cbh34680@gmail.com>
2024-10-22 11:02:04 +0900

    test

--- mysql.hs
+++ mysql.hs
@@ -7,20 +7,33 @@ import qualified System.IO.Streams as Streams
 import Text.Show.Pretty (ppShow)
 import Data.Foldable (for_)
 import Data.Traversable (for)
-import qualified Data.ByteString.Char8 as BS
-
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
+import Control.Exception
+import Data.Time (showGregorian)
 
 main :: IO () 
 main = do
-    conn <- connect
-        defaultConnectInfo {ciUser = "hsdb_user", ciPassword = "hsdb_pass", ciDatabase = "hsdb"}
+    let ci = defaultConnectInfo {ciDatabase="hsdb",
+                ciUser="hsdb_user", ciPassword="hsdb_pass"}
+
+    bracket (connect ci) close dbmain
+
+
+dbmain conn = do
     (defs, is) <- query_ conn "SELECT * FROM dept_manager"
-
-
     rows <- Streams.toList is
-    --putStrLn . ppShow $ rows
+
+    print $ map columnName defs
+
+    for_ rows $ \ [MySQLInt32 emp_no, MySQLText dept_no_t,
+                    MySQLDate from_date_d, MySQLDate to_date_d] -> do
+
+        let dept_no = T.unpack dept_no_t
+        let from_date = showGregorian from_date_d
+        let to_date = showGregorian to_date_d
+
+        print (emp_no, dept_no, from_date, to_date)
 
 
-    let arr = map (zipWith (\def col -> (BS.unpack $ columnName def) ++ "=" ++ show col) defs) $ take 3 rows
-    putStrLn $ ppShow arr
-
+-- EOF

038e880ce07f60a77520d70a7271b9357db6f30a
cbh34680 <cbh34680@gmail.com>
2024-10-27 00:12:05 +0900

    test

--- src/lift2.hs
+++ src/lift2.hs
@@ -1,4 +1,5 @@
 import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Reader
 
@@ -46,16 +47,49 @@ g2 cs = do
 --
 f3 :: StateT Int (ReaderT Int IO) String
 
-f3 = StateT $ \s -> do
+f3 = StateT $ \s ->
         ReaderT $ \r -> do
                 putStr "input3$ "
                 cs <- getLine
 
                 return (cs, s + length cs)
 
---
 
+g3 :: String -> StateT Int (ReaderT Int IO) String
+
+g3 cs = do
+    liftIO $ putStrLn $ concat ["arg3 is [", cs, "]"]
+
+    modify (+1)
+
+    return $ cs ++ "!"
+
+
+--
+f4 :: StateT Int (ReaderT Int IO) String
+
+f4 = StateT $ \s ->
+        ReaderT $ \r ->
+                putStr "input3$ " >>
+
+                    --(\cs -> (cs, s + length cs)) <$> getLine
+                    ((,) <$> id <*> (+s) . length) <$> getLine
+
+
+g4 :: String -> StateT Int (ReaderT Int IO) String
+
+g4 cs = do
+    r <- lift $ ask
+
+    liftIO $ putStrLn $ concat ["arg4 is [", cs, "] r=", show r]
+
+    modify (+1)
+
+    return $ cs ++ "!"
+
+--
 main = do
+    {-
     x1 <- runStateT (f1 >>= g1) 200
     print x1
     putStrLn ""
@@ -64,8 +98,13 @@ main = do
     print x2
     putStrLn ""
 
-    x3 <- runReaderT (runStateT (f3 >>= g2) 200) 300
+    x3 <- runReaderT (runStateT (f3 >>= g3) 200) 300
     print x3
+    putStrLn ""
+    -}
+
+    x4 <- runReaderT (runStateT (f4 >>= g4) 200) 300
+    print x4
     putStrLn ""
 
     putStrLn "done."
